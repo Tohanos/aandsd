aa88439c219f469c5c9ac80c6964a974bbffed5d
Tohanos <kravchenko_ae@mail.ru>
2022-01-20 12:53:34 +0300

    Init commit

---
+++ src/com/company/SelectionSort.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class SelectionSort {
+    public static void sort(Integer[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int min = i;
+            for (int j = min + 1; j < arr.length; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+                
+            }
+
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+
+        }
+    }
+
+}
